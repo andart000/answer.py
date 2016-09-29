d89806a31b2f13169a3b01e10efd295bec32964a
andart000 <perkussionist@gmail.com>
2016-09-29 15:48:31 +0300

    Delete kakdela.py

--- kakdela.py
+++
@@ -1,9 +0,0 @@
-def ask_user():
-    while True:
-        user_say = input('Как дела?')
-        if user_say == 'Хорошо':
-            print('Так бы сразу')
-            break
-        else:
-            print('%s? Подумай получше.' % user_say)
-ask_user()
