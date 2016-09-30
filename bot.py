a38cf9ef64248ca106a19764ee954ea3bb753668
Andrey <perkussionist@gmail.com>
2016-09-30 15:19:32 +0300

    'edited'

--- bot.py
+++ bot.py
@@ -1,10 +1,16 @@
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import answers
 def start(bot, update):
 	print('Вызван /start')
+	bot.sendMessage(update.message.chat_id, text = 'Приветствую, смертный.')
+def talk_to_me(bot, update):
+	print('Новое сообщение: ' + update.message.text)
+	bot.sendMessage(update.message.chat_id, text = answers.try_except())
 def run_bot():
 	updater = Updater('284809150:AAGI1-iIDOOihL0KGb7xFyjqTVE7tepfYbg')
 	dp = updater.dispatcher
 	dp.add_handler(CommandHandler('start', start))
+	dp.add_handler(MessageHandler([Filters.text], talk_to_me))
 	updater.start_polling()
 	updater.idle()
 if __name__ == '__main__':
